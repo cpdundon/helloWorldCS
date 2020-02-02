81c7fd07d8fdd96d7453c82724528797945e5c92
Christopher P. Dundon <chris.dundon@gmail.com>
2020-02-02 09:46:51 -0500

    WORKING debugable code!!!.  It took me close to a week to find the solution!  Not bad for a first cut a new environment.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace helloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string message = "";
+            if (args.Length < 1) {
+                message = "Welcome to .NET Core!";
+            }
+            else {
+                foreach (string item in args) {
+                message += item;
+                }
+            }
+            Console.WriteLine(message);
+            Console.WriteLine(message);
+        }
+    }
+}
